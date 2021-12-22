27ae339c87e8571e133a6f077fdea8fcebb1ea01
CORP\aaparisi <alejandro.aparisi-burgos@cagemini.com>
2021-12-22 12:55:31 +0100

    Add Open Close Principal Example closes #3

---
+++ PrincipiosSolid/OpenClose/Incorrecto/Coche.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrincipiosSolid.OpenClose.Incorrecto
+{
+    public class Coche
+    {
+        private string marca;
+        public string Marca { get; set ; }
+        public Coche(string marca) { this.marca = marca ; }
+        public string getMarcaCoche() { return marca ; }
+    }
+}
---
+++ PrincipiosSolid/OpenClose/Incorrecto/Principal.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrincipiosSolid.OpenClose.Incorrecto
+{
+    public class Principal
+    {
+        public static void main(string[] args)
+        {
+            Coche[] arrayCoches = {
+            new Coche("Renault"),
+            new Coche("Audi")
+            };
+            imprimirPrecioMedioCoche(arrayCoches);
+        }
+
+        public static void imprimirPrecioMedioCoche(Coche[] arrayCoches)
+        {
+            foreach (Coche coche in arrayCoches)
+            {
+                if (coche.Marca.Equals("Renault")) Console.WriteLine(18000);
+                if (coche.Marca.Equals("Audi")) Console.WriteLine(25000);
+            }
+        }
+    }
+}
